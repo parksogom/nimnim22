d2ef3a0d5fb8c28cf95f151b5c216f794e6908b7
jungeun <gomgomno@naver.com>
2024-08-14 17:21:53 +0900

    mit

--- nim2/vue.config.js
+++ nim2/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:"docs",
+  outputDir:"./docs",
   publicPath: process.env.NODE_ENV === "test" ? "test" : "/"   // 레포지스트리(?) 이름쓰기
 
 })
